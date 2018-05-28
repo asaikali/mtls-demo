8494f7cf35c3f84d9d0eac2c299720ed38ef0427
Adib Saikali <asaikali@gmail.com>
2018-05-28 19:32:31 -0400

    Project Skeleton

---
+++ greeting-server/src/main/java/com/example/mtls/greeter/GreetingServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.mtls.greeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingServerApplication.class, args);
+    }
+}
---
+++ greeting-server/src/test/java/com/example/mtls/greeter/GreetingServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mtls.greeter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
